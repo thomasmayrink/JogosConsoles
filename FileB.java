a98a217d6d65d3fe7494b186435573d2d3afae36
Gabriel Mendes <81168814+GabrielMendesM@users.noreply.github.com>
2021-11-13 18:51:14 -0300

    utilização de FileA em FileB na branch 'gabrielBranch'

--- FileB.java
+++ FileB.java
@@ -1,10 +1,12 @@
 public class FileB {
 	FileA fileA;
+	int nome;
 
-	public void methodX(){
+	public void methodX(int nome){
+		fileA.setName(nome);		
 	}
 
 	public void methodY(){
-		FileA.Open();	
+		FileA.open();	
 	}
 }
